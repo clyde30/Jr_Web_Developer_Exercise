4484e74aff177de5cbaf4ef3205b65feb3f87b88
Austin Jones <ajonestx@gmail.com>
2015-10-25 12:40:21 -0600

    add jquery ui accordion

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,4 @@
 for (var chapter in book) {
-	$("#output").append("<h3>Chapter "+chapter+": "+book[chapter].title+"</h3>")
-	$("#output").append("<div><p>"+book[chapter].summary+"</p></div>")
+	$("#accordion").append("<h3>Chapter "+chapter+": "+book[chapter].title+"</h3>")
+	$("#accordion").append("<div><p>"+book[chapter].summary+"</p></div>")
 }
