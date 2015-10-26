c7797f89b305d1a30ef7724ffc2e9100a911e5d0
Austin Jones <ajonestx@gmail.com>
2015-10-25 22:54:52 -0600

    created rotating text box

--- js/myScript.js
+++ js/myScript.js
@@ -2,3 +2,23 @@ for (var chapter in book) {
 	$("#accordion").append("<h3>Chapter "+chapter+": "+book[chapter].title+"</h3>")
 	$("#accordion").append("<div><p>"+book[chapter].summary+"</p></div>")
 }
+
+var textIndex = 1;
+
+function changeText() {
+	textIndex++;
+	$("#banner").fadeIn().html("<p>\""+book[textIndex].summary+"\"</p>").delay(8000).fadeOut();
+	if (textIndex >= chapter) {
+	textIndex = 0;
+	};	
+
+}
+
+function banner() {
+	$("#banner").html("<p>\""+book[1].summary+"\"</p").delay(8000).fadeOut();
+	setInterval(changeText,10000);
+}
+
+ $(document).ready(function () {
+	banner();
+})
